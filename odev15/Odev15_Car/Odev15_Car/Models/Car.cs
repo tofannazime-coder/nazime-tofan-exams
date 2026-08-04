d007b4fce3fa00193897fe9226f34771e0b211f6
Nazime <tofannazime@gmail.com>
2026-08-04 22:12:57 +0300

    Odev15 Car Projesinin Geliştirilmesi Tamamlandı
    Odev15 Car Projesinin Geliştirilmesi Tamamlandı

---
+++ odev15/Odev15_Car/Odev15_Car/Models/Car.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Odev15_Car.Models;
+
+public class Car
+{
+    public String Brand { get; set; }
+    public String Model { get; set; }
+    public int Year { get; set; }
+    public String PlateNumber { get; set; }
+    public String Color { get; set; }
+}
---
+++ odev15/Odev15_Car/Odev15_Car/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Drawing;
+using Odev15_Car.Models;
+namespace Odev15_Car;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<Car> cars = new List<Car>
+        {
+            new Car
+            {
+                Brand= "BMW",
+                Model="F30",
+                Year=2019,
+                PlateNumber="34 BTL 554",
+                Color="Gri"
+            },
+             new Car
+            {
+                Brand="Honda",
+                Model="Civic",
+                Year=2020,
+                PlateNumber="41 GHT 963",
+                Color="Kırmızı"
+            },
+             new Car
+            {
+                Brand="Renault",
+                Model="Clio",
+                Year=2019,
+                PlateNumber="63 PNK 878",
+                Color="Beyaz"
+            },
+        };
+        Console.WriteLine("Arabalar: ");
+        Console.WriteLine("**********");
+        foreach (var car in cars)
+        {
+            Console.WriteLine($"Marka: {car.Brand}");
+            Console.WriteLine($"Model: {car.Model}");
+            Console.WriteLine($"Yıl: {car.Year}");
+            Console.WriteLine($"Plaka: {car.PlateNumber}");
+            Console.WriteLine($"Renk: {car.Color}");
+            Console.WriteLine();
+        }
+    }
+}
