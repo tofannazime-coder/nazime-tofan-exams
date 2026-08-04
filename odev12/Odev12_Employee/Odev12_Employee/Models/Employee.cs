0ca741d416b069021c4e50a9b7ed1fc31eadfeac
Nazime <tofannazime@gmail.com>
2026-08-04 16:15:51 +0300

    Odev12 Employee Projesinin Geliştirilmesi Tamamlandı
    Odev12 Employee Projesinin Geliştirilmesi Tamamlandı

---
+++ odev12/Odev12_Employee/Odev12_Employee/Models/Employee.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Odev12_Employee.Models;
+
+
+    public class Employee
+    {
+        public string FullName { get; set; }
+        public string Title { get; set; }
+        public string Department { get; set; }
+        public DateTime HireDate { get; set; }
+    }
---
+++ odev12/Odev12_Employee/Odev12_Employee/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Odev12_Employee.Models;
+
+namespace Odev12_Employee
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee b1 = new Employee {FullName = "Nazlı Tofan" , Title = "Uzman" , Department="Yazılım", HireDate=DateTime.Now.AddMonths(-1)};
+            Employee b2 = new Employee {FullName = "Hatice Kübra Kaya" , Title = "Bilgisayar Proğramcılığı" , Department="Yazılım Geliştirme", HireDate=DateTime.Now.AddYears(-2)};
+            Employee b3 = new Employee {FullName = "Baran Bal" , Title = "İşletme Öğrencisi" , Department="Yönetim ve Organizasyon", HireDate=DateTime.Now.AddMonths(-7)};
+
+            Employee[] employees = {b1 , b2 , b3};
+
+            foreach(var employee in employees)
+            {
+                Console.WriteLine($" Çalışan: {employee.FullName} | Unvan: {employee.Title} | Departman: {employee.Department}");
+            }
+        }
+    }
+}
